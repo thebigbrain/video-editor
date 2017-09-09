aef425f429ec6014f2cbd79d5324a0e555dbab95
jiangjianyi <jiangjianyi@jrh2016.com>
2017-09-09 23:09:09 +0800

    。

--- js/index.js
+++ js/index.js
@@ -15,8 +15,9 @@ import FFMpeg from './ffmpeg';
 
 const FilterMap = {
   'boxblur': 'boxblur=2:1:cr=0:ar=0',
-  'colorbalance': 'colorbalance=rs=.3',
-  'colorchannelmixer': 'colorchannelmixer=.3:.4:.3:0:.3:.4:.3:0:.3:.4:.3',
+  // 'colorchannelmixer': 'colorchannelmixer=.3:.4:.3:0:.3:.4:.3:0:.3:.4:.3',
+  'drawbox': 'drawbox=color=pink@0.5:t=max',
+  'drawgrid': 'drawgrid=width=100:height=100:thickness=2:color=red@0.5'
 };
 
 export default class VE extends Component {
@@ -66,7 +67,7 @@ export default class VE extends Component {
   onPressAddFilter () {
     let source = this.cameraPickerRef.video.path;
     let target = `VE-${new Date().getTime()}.mp4`;
-    FFMpeg.run(`-i ${source} -vf ${FilterMap.colorchannelmixer} ${target}`);
+    FFMpeg.run(`-i ${source} -vf ${FilterMap.drawbox} ${target}`);
   }
 
   render() {
